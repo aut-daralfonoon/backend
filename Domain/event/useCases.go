87e86e661a55585acdf8dc72c6efcae81246639a
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-21 22:00:31 +0430

    Change EventManager Methods parameter and Outputs
    -Adds services.go

--- Domain/event/useCases.go
+++ Domain/event/useCases.go
@@ -1,14 +1,14 @@
 package event
 
-type EventManager interface{
-	Event()
-	Events()
+type EventManager interface {
+	Event(Id uint) (*Event, error)
+	Events() ([]*Event, error)
 	AddEvent()
 	ModifyEvent()
 	PublishEvent()
 	ArchiveEvent()
 	DeleteEvent()
-	Register()
-	SingleSubjectRegister()
+	Register() 
+	SingleSubjectRegister(eventId uint, userEmail string) error
 	GetEventParticipants()
 }
