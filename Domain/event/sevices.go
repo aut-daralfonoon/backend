d1a92d4aec3437f51d85794f67e397ec1e5eab3a
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-22 14:23:12 +0430

    Add eventManager
    -Adds eventManager services

--- Domain/event/sevices.go
+++ Domain/event/sevices.go
@@ -1,3 +1,48 @@
 package event
 
+type eventManager struct {
+}
 
+func NewEventManger() EventManager {
+	return &eventManager{}
+}
+
+func (em *eventManager) Event(Id uint) (*EventDto, error) {
+	return nil, nil
+}
+
+func (em *eventManager) Events() ([]*EventDto, error) {
+	return nil, nil
+}
+
+func (em *eventManager) AddEvent() {
+
+}
+
+func (em *eventManager) ModifyEvent() {
+
+}
+
+func (em *eventManager) PublishEvent() {
+
+}
+
+func (em *eventManager) ArchiveEvent() {
+
+}
+
+func (em *eventManager) DeleteEvent() {
+
+}
+
+func (em *eventManager) Register() {
+
+}
+
+func (em *eventManager) SingleSubjectRegister(eventId uint, userEmail string) error {
+	return nil
+}
+
+func (em *eventManager) GetEventParticipants() {
+
+}
