1a15793e22cee2fd4ee3283d4be597d2835f2204
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-22 21:00:46 +0430

    Update Presentation Model

--- Domain/event/presentation.go
+++ Domain/event/presentation.go
@@ -3,12 +3,21 @@ package event
 import "time"
 
 type Presentation struct {
-	Id          uint
-	EventId     uint
-	Title       uint
-	Start       time.Time
-	End         time.Time
-	Archived_at time.Time
-	Created_at  time.Time
-	Updated_at  time.Time
+	Id            uint
+	EventId       uint
+	Title         uint
+	SessionStatus SessionStatus
+	Start         time.Time
+	End           time.Time
+	Archived_at   time.Time
+	Created_at    time.Time
+	Updated_at    time.Time
 }
+
+type SessionStatus string
+
+const (
+	ClosedToJoin    SessionStatus = "closed_to_join"
+	OpenToJoin      SessionStatus = "open_to_join"
+	ArchivedSession SessionStatus = "archived"
+)
