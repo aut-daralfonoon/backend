563d61232e9f6cc611ce4079bbd807f97bf1c72a
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-22 15:39:19 +0430

    Update Event Model

--- Domain/event/event.go
+++ Domain/event/event.go
@@ -1,12 +1,26 @@
 package event
 
-import "time"
+import (
+	"time"
+)
 
 type Event struct {
-	Id          uint
-	Title       uint
-	Description string
-	Date        time.Time
-	Enable      bool
-	Poster      string
+	Id           uint
+	Title        uint
+	Event_Status Event_Status
+	Start        time.Time
+	End          time.Time
+	Description  string
+	Poster       string
+	Archived_at  time.Time
+	Created_at   time.Time
+	Updated_at   time.Time
 }
+
+type Event_Status string
+
+const (
+	INACTIVE Event_Status = "inactive"
+	OPEN     Event_Status = "open"
+	ARCHIVED Event_Status = "archived"
+)
