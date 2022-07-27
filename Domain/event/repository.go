9e0b4bff7577a7e951abc9b86f010f020d2d6172
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-27 22:41:06 +0430

    Edit Repository Read Functions Outputs

--- Domain/event/repository.go
+++ Domain/event/repository.go
@@ -1,14 +1,15 @@
 package event
 
-
-func CreateEvent(event *Event) error{
+func CreateEvent(event *Event) error {
 	return nil
 }
 
-func ReadEvent(event *Event) error{
-	return nil
+func ReadEvent() (*Event, error) {
+	var event *Event
+	return event,nil
 }
 
-func ReadEvents(events []*Event) error{
-	return nil
+func ReadEvents() ([]*Event, error) {
+	var events []*Event
+	return events,nil
 }
