01e8529970780ab58c317470fb40a73787891aa5
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-07-30 14:12:00 +0430

    Add Presentation Services
    -Defines Presentations
    -Defines PresentationsByEvent
    -Defines AddPresentation
    -Defines EditPresemtation
    -Defines DeletePresentation

--- Domain/event/presentation_service.go
+++ Domain/event/presentation_service.go
@@ -4,3 +4,23 @@ package event
 func Presentation(ID uint) (*PresentationDto, error){
 	return nil,nil
 }
+
+func Presentations()([]*PresentationDto, error){
+	return nil,nil
+}
+
+func PresentationsByEvent(eventID uint)([]*PresentationDto, error){
+	return nil,nil
+}
+
+func AddPresentation(pr *PresentationDto) error{
+	return nil
+}
+
+func EditPresentation(pr *PresentationDto) error{
+	return nil
+}
+
+func DeletePresentation(ID uint) error{
+	return nil
+}
