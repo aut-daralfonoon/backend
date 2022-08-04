038ddb3b02081ec3204e573bef5b5a8d787dfd1e
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-08-04 21:46:06 +0430

    fix/holding/event models

--- modules/holding/event/models.go
+++ modules/holding/event/models.go
@@ -1,8 +1,33 @@
 package event
 
-import "time"
+import (
+	"time"
+)
 
-type PresentationModel struct {
+type HldEvent struct {
+	Id           uint
+	Title        uint
+	Event_Status Event_Status
+	Start        time.Time
+	End          time.Time
+	Description  string
+	Poster       string
+	Archived_at  time.Time
+	Created_at   time.Time
+	Updated_at   time.Time
+}
+
+type Event_Status string
+
+const (
+	INACTIVE Event_Status = "inactive"
+	OPEN     Event_Status = "open"
+	ARCHIVED Event_Status = "archived"
+)
+
+
+
+type HldPresentation struct {
 	Id                uint
 	EventId           uint
 	Title             uint
