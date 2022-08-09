48cea558881a80b8b7ad27b9a1861c7c0c10c50f
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-08-09 12:27:16 +0430

    feature/api gin router groups

--- gateway/api/api.go
+++ gateway/api/api.go
@@ -1,3 +1,13 @@
 package api
 
+import (
+	"github.com/AUT-Daralfonoon/back-end/gateway/api/holding"
+	"github.com/gin-gonic/gin"
+)
 
+
+func AddRoutes(master *gin.RouterGroup){
+	r:= master.Group("/api", nil)
+
+	holding.AddRoutes(r)
+}
